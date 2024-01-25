2ee897cadfb4c1d0ca259c10b358af624a8ad54c
Kazuki95-hub <78989081+Kazuki95-hub@users.noreply.github.com>
2024-01-25 23:24:21 +0900

    tailwindCSSの導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,11 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact()],
+  jsx: 'react',
+  devOptions: {
+    // port: 3000,         // The port to run the dev server on.
+    tailwindConfig: './tailwind.config.js', // Path to tailwind.config.js if used, e.g. './tailwind.config.js'
+  },
 });
+
